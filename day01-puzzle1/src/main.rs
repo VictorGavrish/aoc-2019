ccf93f9065c6842c1328c2e26e43e5d44ff3f30d
Victor Gavrish <victor.gavrish@gmail.com>
2019-12-01 18:35:24 +0300

    add solution to day 1 puzzle 1

---
+++ day01-puzzle1/src/main.rs
@@ -0,0 +1,27 @@
+static INPUT: &'static str = include_str!("../resources/input.txt");
+
+fn main() {
+    let sum: u32 = INPUT
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .map(|mass| calc_fuel(mass))
+        .sum();
+    println!("{}", sum);
+}
+
+fn calc_fuel(mass: u32) -> u32 {
+    mass / 3 - 2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn tests() {
+        assert_eq!(calc_fuel(12), 2);
+        assert_eq!(calc_fuel(14), 2);
+        assert_eq!(calc_fuel(1969), 654);
+        assert_eq!(calc_fuel(100756), 33583);
+    }
+}
